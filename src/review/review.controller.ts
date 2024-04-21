3f377d06aa8958e8cd9f1d339721c38a7f6135b6
Evgeniy Ehanovskyi <yehanovskiy@gmail.com>
2024-04-21 19:18:32 +0300

    add user email decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -3,6 +3,8 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/auth/decorators/user-email.decorator';
+import { getConfigToken } from '@nestjs/config';
 
 @Controller('review')
 export class ReviewController {
@@ -14,7 +16,7 @@ export class ReviewController {
 	async create(@Body() dto: CreateReviewDto) {
 		return this.reviewService.create(dto)
 	}
-
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
 		const deleteDoc = await this.reviewService.delete(id)
@@ -27,7 +29,8 @@ export class ReviewController {
 
 	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async getByProductId(@Param('productId') productId: string) {
+	async getByProductId(@Param('productId') productId: string, @UserEmail() email: string) {
+		console.log(email)
 		return this.reviewService.findByProductId(productId)
 	}
 
