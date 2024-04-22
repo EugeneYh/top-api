e084be527a42de0ef3110dfd65b16e38aee624db
Evgeniy Ehanovskyi <yehanovskiy@gmail.com>
2024-04-22 23:15:45 +0300

    add id validation

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -4,6 +4,7 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserEmail } from '../auth/decorators/user-email.decorator';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -17,7 +18,7 @@ export class ReviewController {
 	}
 	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deleteDoc = await this.reviewService.delete(id)
 		if (!deleteDoc) {
 			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
@@ -25,7 +26,7 @@ export class ReviewController {
 	}
 
 	@Get('byProduct/:productId')
-	async getByProductId(@Param('productId') productId: string, @UserEmail() email: string) {
+	async getByProductId(@Param('productId', IdValidationPipe) productId: string, @UserEmail() email: string) {
 		return this.reviewService.findByProductId(productId)
 	}
 }
